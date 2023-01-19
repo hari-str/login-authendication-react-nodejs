8bf3cbcae32683148e305fa17920d73d755c7e25
Haristr <haristr5235@gmail.com>
2023-01-19 14:33:30 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const userRouter = require("./Router/userRouter");
 const { PORT, MONGODB_URL } = process.env;
 
 //mongodb connect
+mongoose.set("strictQuery", false);
 mongoose
   .connect(MONGODB_URL)
   .then(() => {
@@ -20,7 +21,12 @@ mongoose
 
 //middleware
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://login-signup-app-api.onrender.com",
+  })
+);
 app.use(cookieParser());
 app.use("/api", userRouter);
 app.use((err, req, res, next) => {
